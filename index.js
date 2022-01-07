510d3609544150a7dfebeeddb367c96675d85bf4
Michael Iringan <miringan@uw.edu>
2022-01-06 20:33:25 -0800

    Added npm modules. Added code to index.js.

---
+++ index.js
@@ -0,0 +1,61 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "githubName",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is your project's name?",
+      name: "projectName",
+    },
+    {
+      type: "input",
+      message: "Please write a short description of your project.",
+      name: "description",
+    },
+    {
+      type: "list",
+      message: "What kind of license should your project have?",
+      name: "license",
+      choices: ['MIT', 'Apache', 'BSD', 'GPL', 'Public Domain'],
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "dependency",
+        default: "npm i",
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "tests",
+        default: "npm test",
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "needToKnowUse",
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "needToKnowContributions",
+    },
+  ])
+  .then((data) => {
+    fs.writeFile("generatedREADME.md", JSON.stringify(data), (err) =>
+    err ? console.error(err) : console.log('Success!')
+    );
+  })
+
+// Need to add a template.js file and do module exports.
